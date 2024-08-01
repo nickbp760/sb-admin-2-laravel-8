2a8c51a49d1493e7846c959ea8d594698a33bb78
nickbp760 <nikochristian760@gmail.com>
2024-08-02 00:56:47 +0700

    Translate to Indo

--- resources/views/jemaat/index.blade.php
+++ resources/views/jemaat/index.blade.php
@@ -45,17 +45,17 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="delete-modalLabel">Confirm Deletion</h5>
+                <h5 class="modal-title" id="delete-modalLabel">Konfirmasi Hapus</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                Are you sure you want to delete this item?
+                Apakah Anda Benar-Benar Ingin Menghapus Item Ini?
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-danger" id="confirm-delete">Delete</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
+                <button type="button" class="btn btn-danger" id="confirm-delete">Ya Hapus</button>
             </div>
         </div>
     </div>
